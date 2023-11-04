bff470e6b59555c8a106f874af210b51a6b31b4e
John-Davidson-8 <jodo828@gmail.com>
2023-11-04 11:15:17 +0000

    Add function which returns computers choice using a variation of the math.random function

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+// Our first task for creating a game of Rock, Paper, Scissors between the computer and competitor is to write a function that allows the computer to randomly pick one three elements that are contained within the array variable items. The random return value and syntax is new to me it is variation of tbe math-random function.
+
+function getComputerChoice(items) {
+  return items[Math.floor(Math.random() * items.length)];
+}
+
+var items = ["Rock", "Paper", "Scissors"];
+console.log(getComputerChoice(items));
